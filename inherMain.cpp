9db1aead00db96e229717ae46f3827028d44c3c1
Nicholas <126486825+SnackPack678@users.noreply.github.com>
2025-07-13 18:47:06 -0700

    Add files via upload

---
+++ inherMain.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  inheritanceMethodOverriding
+//
+//  Created by Nicholas Rubio on 7/13/25.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// Base class
+class User {
+public:
+    virtual void accessLevel() {
+        cout << "General Access\n";
+    }
+};
+
+// Derived from User
+class Employee : public User {
+public:
+    void accessLevel() override {
+        cout << "Employee Access\n";
+    }
+};
+
+// Derived from Employee
+class InventoryManager : public Employee {
+public:
+    void accessLevel() override {
+        cout << "Full Inventory Management Access\n";
+    }
+};
+
+int main() {
+    InventoryManager mgr;
+    mgr.accessLevel(); // Should print: Full Inventory Management Access
+    return 0;
+}
+
+
+
