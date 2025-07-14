6c93124eef7c16cebe6d4295ebc6cf4502c9a3bd
Nicholas <126486825+SnackPack678@users.noreply.github.com>
2025-07-13 18:45:29 -0700

    Assignment-10

--- main.cpp
+++ main.cpp
@@ -1,41 +1,36 @@
 //
 //  main.cpp
-//  inheritanceMethodOverriding
+//  organizingCodeWithHeaderFiles
 //
 //  Created by Nicholas Rubio on 7/13/25.
 //
 
 #include <iostream>
+#include "Item.h"
+
+Item::Item() {
+    name = "Unknown";
+    quantity = 0;
+}
+
+Item::Item(string itemName, int itemQty) {
+    name = itemName;
+    quantity = itemQty;
+}
+
+void Item::display() {
+    cout << "Item: " << name << ", Quantity: " << quantity << endl;
+}
 
 using namespace std;
 
-// Base class
-class User {
-public:
-    virtual void accessLevel() {
-        cout << "General Access\n";
-    }
-};
-
-// Derived from User
-class Employee : public User {
-public:
-    void accessLevel() override {
-        cout << "Employee Access\n";
-    }
-};
-
-// Derived from Employee
-class InventoryManager : public Employee {
-public:
-    void accessLevel() override {
-        cout << "Full Inventory Management Access\n";
-    }
-};
-
 int main() {
-    InventoryManager mgr;
-    mgr.accessLevel(); // Should print: Full Inventory Management Access
+    Item item1("Notebook", 10);
+    Item item2("Pencil", 50);
+
+    item1.display();
+    item2.display();
+
     return 0;
 }
 
